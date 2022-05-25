bb5c6c0495b5855d7a821b2076eaca9ed95a3e2e
juancolson <87849544+juancolson@users.noreply.github.com>
2022-05-25 14:58:00 +0545

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,6 @@ const Home = () => {
 
     var response = {}
 
-    // if (canParse) {
     response["title"] = sample.title.pretty
 
 
@@ -42,28 +41,30 @@ const Home = () => {
     console.log(data);
     var a = document.getElementById("dwn")
     a.href = 'data:' + data
+    a.download = sample.title.pretty + '.json'
 
 
     // }
-  }
+  };
+
   return (
-    <div className="w-screen h-screen">
+    <div className="w-screen h-screen flex flex-col">
 
       <form onSubmit={submittedNum} className="m-3 w-11/12">
-        <input type="number" name="number" className="border border-black" />
-        <button type="submit" className="rounded-xl bg-blue-700 w-9">GO</button>
+        <input type="number" name="number" className="border border-black w-full" />
+        <button type="submit" className="rounded bg-blue-700 text-white p-2 w-full">GO</button>
       </form>
-      <button className="rounded bg-blue-700 text-white" onClick={() => {
+      <button className="rounded bg-blue-700 text-white p-2" onClick={() => {
         navigator.clipboard.readText().then((clipText) => (document.getElementById("paste").value = clipText));
       }}>PASTE</button>
       <form onSubmit={submittedForm} className="m-3 w-11/12">
-        <textarea name="data" id="paste" className="border border-black w-full h-60 min-h-[50%]" />
-        <button type="submit" className="rounded-xl bg-blue-700 w-9">GO</button>
+        <textarea name="data" id="paste" className="border border-black w-full h-60 min-h-[50%] p-2" />
+        <button type="submit" className="rounded-xl bg-blue-700 w-full p-2">GO</button>
       </form>
 
 
-      <textarea id="resp" className="border m-3 border-black w-11/12 h-3/6" />
-      <button><a download={'details.json'} id="dwn" className="rounded-xl bg-blue-700">Download</a></button>
+      <textarea id="resp" className="border m-3 border-black w-11/12 h-3/6 p-2" />
+      <button className="rounded bg-blue-700 text-white p-2 w-full"><a id="dwn" >Download</a></button>
     </div>
   );
 }
